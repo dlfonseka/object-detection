c5c37418724c97574bee459b17ced226a8032fd3
Kevin Black <kevin3.black@gmail.com>
2020-01-07 19:28:03 -0600

    Fix setup.py packages

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ os.chdir(cwd)
 if not os.path.exists("object_detection"):
     os.symlink(".", "object_detection")
 subprocess.call(["protoc object_detection/protos/*.proto --python_out=."], shell=True)
+os.unlink("object_detection")
 
 setup(
     name='object-detection',
     version='0.0.1',
     description='Tensorflow object detection API',
     package_dir={'object_detection': '.'},
-    packages=['object_detection.' + p for p in find_packages()],
+    packages=['object_detection.' + p for p in find_packages()] + ['object_detection'],
     install_requires=[
-        'tensorflow>=1.12.0',
         'Cython',
         'contextlib2',
         'pillow',
